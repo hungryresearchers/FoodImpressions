ce8a6afeb1d4ee09f7b84b2d4e95c655468981b0
hiroki <hata@whatzmoney.com>
2017-04-04 11:38:44 +0900

    Write routing

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,17 @@
 class PostController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find_by( post_id: params[:id] )
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      flash[:success] = t('flash.destroy')
+      format.html { redirect_to root}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root
+  get '/static/index'
+  root 'static#index'
+
+  get '/new' => 'post#new'
+  get '/show' => 'post#show'
+
+  resources :post
 end
